5a941f2844f34fd5209fac925f8f1ad9250cc9a9
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-26 21:16:06 +0500

    Styling component

--- src/app/wish-list-item/wish-list-item.component.ts
+++ src/app/wish-list-item/wish-list-item.component.ts
@@ -1,9 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-wish-list-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './wish-list-item.component.html',
   styleUrl: './wish-list-item.component.css',
 })
@@ -11,6 +12,11 @@ export class WishListItemComponent {
   @Input() wishText!: string;
   @Input() fullFilled!: boolean;
   @Output() fullFilledChange = new EventEmitter<boolean>();
+
+  get cssClasses() {
+    // return this.fullFilled ? ['strikeout', 'text-muted'] : [];
+    return { 'strikeout text-muted': this.fullFilled };
+  }
 
   toggleFullFilled() {
     this.fullFilled = !this.fullFilled;
