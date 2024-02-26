fbc7ccffe206dc24161d9f3b036e6c17ece7999d
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-26 17:56:46 +0500

    Ngmodel for data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { WishItem } from './shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  items: WishItem[] = [
-    new WishItem('Learn Angular 2'),
-    new WishItem('Get a job', true),
-    new WishItem('Start a company'),
-  ];
+  items: WishItem[] = [];
   title = 'wishlist';
+  newWishText = '';
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
