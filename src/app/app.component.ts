298398335f20206a2031a364637768c1fd15eb89
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-26 16:11:05 +0500

    Added wish item model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from './shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  items = [
+    new WishItem('Learn Angular 2'),
+    new WishItem('Get a job', true),
+    new WishItem('Start a company'),
+  ];
   title = 'wishlist';
 }
