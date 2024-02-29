1a9138dfebd622242ac8751d3fa09f64bae216eb
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-29 21:19:13 +0500

    Handling not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { WishComponent } from './wish/wish.component';
 import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
