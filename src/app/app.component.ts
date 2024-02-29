cb3c8f1a0ae9dae2e49b68d07921e0f5a8ef7262
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-02-29 21:01:50 +0500

    Defining routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { WishModule } from './wish/wish.module';
 import { ContactModule } from './contact/contact.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [WishModule, ContactModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WishComponent } from './wish/wish.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WishComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
